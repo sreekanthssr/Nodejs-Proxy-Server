6bde9d8ec47de685be7723711399a46b6fa2fa89
sreekanth s <sreekanthssr@gmail.com>
2023-02-10 03:11:29 +0530

    Es lint added

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+/* eslint-disable import/extensions */
 import getAPIConfig from './common/get-api-config.js';
-import valiateAPIConfig from'./common/api-validator.js';
-import handelAPI from './common/handel-api.js';
+import valiateAPIConfig from './common/api-validator.js';
+import handelAPI from './common/handle-api.js';
 import logMessage from './common/log.js';
 
-async function startProxyServer({configFile, app}){
+async function startProxyServer({ configFile, app }) {
   const apiConfig = await getAPIConfig(configFile);
-  if(valiateAPIConfig(apiConfig)){
-    app.all('**', function (req, res) {
-      handelAPI(req,res,apiConfig);
-    })
+  if (valiateAPIConfig(apiConfig)) {
+    app.all('**', (req, res) => {
+      handelAPI(req, res, apiConfig);
+    });
   } else {
-    logMessage("Please check the api configuration");
+    logMessage('Please check the api configuration');
   }
 }
 
 export default startProxyServer;
-
-
-
-
-
